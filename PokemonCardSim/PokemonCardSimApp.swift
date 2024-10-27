afd7313e5aff18038aeb598057e650d50f839405
SamP10 <sam.plant998@gmail.com>
2024-10-27 22:43:54 +0000

    Initial Commit

---
+++ PokemonCardSim/PokemonCardSimApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonCardSimApp.swift
+//  PokemonCardSim
+//
+//  Created by Sam Plant on 27/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct PokemonCardSimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
