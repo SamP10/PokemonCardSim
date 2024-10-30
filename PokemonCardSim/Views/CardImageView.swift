fcc987c272a3e99f4473cc660ea10c0f45e5c34e
SamP10 <sam.plant998@gmail.com>
2024-10-30 23:50:41 +0000

    Add background

--- PokemonCardSim/Views/CardImageView.swift
+++ PokemonCardSim/Views/CardImageView.swift
@@ -12,7 +12,6 @@ import UIKit
 struct CardImageView: View {
     @State private var offset = CGSize.zero
     @State private var zIndex: Double = 0;
-    @State private var rotationOffset: CGSize = .zero;
     private var uiImage: UIImage;
     var onTap: (() -> Double);
 
@@ -34,47 +33,7 @@ struct CardImageView: View {
                         self.offset = CGSize(width: 250, height: 0)
                     }
                 }
+                .shadow(color: Color(red: 0.5, green: 0.5, blue: 0.5, opacity: 0.3), radius: 7.5, x: 2, y: 2)
                 .zIndex(self.zIndex)
-                .gesture(
-                        DragGesture()
-                            .onChanged { value in
-                                if(self.offset == .zero) {
-                                    self.rotationOffset = value.translation
-                                }
-                            }
-                            .onEnded { _ in
-                                withAnimation(
-                                    .interactiveSpring(
-                                        response: 0.6,
-                                        dampingFraction: 0.32,
-                                        blendDuration: 0.32))
-                                {
-                                    self.rotationOffset = .zero
-                                }
-                            }
-                    )
-                .rotation3DEffect(
-                    self.offset2Angle(),
-                    axis: (x: 1, y: 0, z: 1)
-                )
-                .rotation3DEffect(
-                    self.offset2Angle(true),
-                    axis: (x: 0, y: 1, z: 1)
-                )
     }
-    
-    func offset2Angle(_ isVerticle: Bool = false) -> Angle {
-        let degree = (isVerticle ? self.rotationOffset.height : self.rotationOffset.width) /
-            (isVerticle ? self.screenSize.height : self.screenSize.width)
-        return .init(degrees: degree * 20)
-    }
-    
-    var screenSize: CGSize = {
-        guard let window = UIApplication.shared.connectedScenes.first as? UIWindowScene
-        else {
-            return .zero
-        }
-        
-        return window.screen.bounds.size
-    }()
 }
--- PokemonCardSim/Views/CardSetView.swift
+++ PokemonCardSim/Views/CardSetView.swift
@@ -16,6 +16,7 @@ struct CardSetView: View {
     @State private var cardPack: [PokemonCard] = []
     @State private var setId: String;
     @State private var zIndex: Double = 0;
+    @State private var rotationOffset: CGSize = .zero;
     
     init(setId: String) {
         self.setId = setId;
@@ -29,12 +30,40 @@ struct CardSetView: View {
                     .frame(width: 50, height: 50)
             } else {
                 ZStack{
+                    Image("Background")
+                        .resizable()
+                        .scaledToFill()
                     ForEach(self.cardPack.reversed(), id: \.id) { card in
                         CardImageView(uiImage: self.imageService.cardByImage[card] ?? UIImage()){
                             self.zIndex += 1
                             return self.zIndex
                         }
                     }
+                    .gesture(
+                        DragGesture()
+                            .onChanged { value in
+                                self.rotationOffset = value.translation
+                            }
+                            .onEnded { _ in
+                                withAnimation(
+                                    .interactiveSpring(
+                                        response: 0.6,
+                                        dampingFraction: 0.32,
+                                        blendDuration: 0.32))
+                                {
+                                    self.rotationOffset = .zero
+                                }
+                            }
+                    )
+                    .rotation3DEffect(
+                        self.offset2Angle(),
+                        axis: (x: 10, y: 0, z: 2)
+                    )
+                    .rotation3DEffect(
+                        self.offset2Angle(true),
+                        axis: (x: 0, y: 1, z: 2)
+                    )
+
                 }
             }
         }
@@ -49,4 +78,19 @@ struct CardSetView: View {
             }
         }
     }
+    
+    func offset2Angle(_ isVerticle: Bool = false) -> Angle {
+        let degree = (isVerticle ? self.rotationOffset.height : self.rotationOffset.width) /
+            (isVerticle ? self.screenSize.height : self.screenSize.width)
+        return .init(degrees: degree * 20)
+    }
+    
+    var screenSize: CGSize = {
+        guard let window = UIApplication.shared.connectedScenes.first as? UIWindowScene
+        else {
+            return .zero
+        }
+        
+        return window.screen.bounds.size
+    }()
 }
