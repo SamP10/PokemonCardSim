1c14e0e86b96fdf6704e48943c96376a19bc3e2c
SamP10 <sam.plant998@gmail.com>
2024-10-30 23:14:44 +0000

    add cooler animation

--- PokemonCardSim/Views/CardImageView.swift
+++ PokemonCardSim/Views/CardImageView.swift
@@ -12,10 +12,7 @@ import UIKit
 struct CardImageView: View {
     @State private var offset = CGSize.zero
     @State private var zIndex: Double = 0;
-    @State private var degrees: Double = 0;
-    @State private var x: Double = 0;
-    @State private var y: Double = 0
-    @State private var z: Double = 0
+    @State private var rotationOffset: CGSize = .zero;
     private var uiImage: UIImage;
     var onTap: (() -> Double);
 
@@ -26,46 +23,56 @@ struct CardImageView: View {
     }
     
     var body: some View {
-        Image(uiImage: uiImage)
-            .resizable()
-            .scaledToFit()
-            .frame(maxWidth: 200)
-            .offset(self.offset)
-            .onTapGesture {
-                self.offset = CGSize(width: 250, height: 0)
-                self.zIndex = onTap()
-            }
-            .animation(.smooth, value: self.offset)
-            .zIndex(self.zIndex)
-            .gesture(
-                    DragGesture()
-                        .onChanged { value in
-                            if(value.translation.width > -45 &&
-                               value.translation.width < 45 &&
-                               value.translation.height > -45 &&
-                               value.translation.height < 45 &&
-                               self.offset == CGSize.zero
-                            ) {
-                                self.degrees = Double((value.translation.width + value.translation.height) / 2)
+            Image(uiImage: uiImage)
+                .resizable()
+                .scaledToFit()
+                .frame(maxWidth: 200)
+                .offset(self.offset)
+                .onTapGesture {
+                    self.zIndex = onTap()
+                    withAnimation(.smooth) {
+                        self.offset = CGSize(width: 250, height: 0)
+                    }
+                }
+                .zIndex(self.zIndex)
+                .gesture(
+                        DragGesture()
+                            .onChanged { value in
+                                self.rotationOffset = value.translation
                             }
-
-                            self.x = value.translation.height
-                            self.y = value.translation.width
-                            self.z = (self.x + self.y) / 2
-                            print(self.x)
-                            print(self.y)
-                        }
-                        .onEnded { value in
-                            self.degrees = 0
-                            self.x = 0
-                            self.y = 0
-                            self.z = 0
-                        }
+                            .onEnded { _ in
+                                withAnimation(
+                                    .interactiveSpring(
+                                        response: 0.6,
+                                        dampingFraction: 0.32,
+                                        blendDuration: 0.32))
+                                {
+                                    self.rotationOffset = .zero
+                                }
+                            }
+                    )
+                .rotation3DEffect(
+                    self.offset2Angle(),
+                    axis: (x: 1, y: 0, z: 1)
                 )
-            .rotation3DEffect(
-                .degrees(self.degrees),
-                axis: (x: self.x, y: self.y, z: self.z),
-                anchor: .center
-            )
+                .rotation3DEffect(
+                    self.offset2Angle(true),
+                    axis: (x: 0, y: 1, z: 1)
+                )
     }
+    
+    func offset2Angle(_ isVerticle: Bool = false) -> Angle {
+        let progress = (isVerticle ? self.rotationOffset.height : self.rotationOffset.width) /
+            (isVerticle ? self.screenSize.height : self.screenSize.width)
+        return .init(degrees: progress * 10)
+    }
+    
+    var screenSize: CGSize = {
+        guard let window = UIApplication.shared.connectedScenes.first as? UIWindowScene
+        else {
+            return .zero
+        }
+        
+        return window.screen.bounds.size
+    }()
 }
