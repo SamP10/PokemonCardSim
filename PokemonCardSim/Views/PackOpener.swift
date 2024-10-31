874832de8da79fa2c5f641fa0a5c94cdbeb53029
SamP10 <sam.plant998@gmail.com>
2024-10-31 22:03:28 +0000

    Move view down

---
+++ PokemonCardSim/Views/PackOpener.swift
@@ -0,0 +1,85 @@
+//
+//  PackOpener.swift
+//  PokemonCardSim
+//
+//  Created by Sam Plant on 31/10/2024.
+//
+
+import SwiftUI
+import CoreData
+import UIKit
+
+struct PackOpener: View {
+    @State private var imageService: ImageService;
+    @State private var cardPack: [PokemonCard] = [];
+    @State private var zIndex: Double = 0;
+    @State private var rotationOffset: CGSize = .zero;
+    @State private var revealedCards: [PokemonCard] = [];
+    @Binding var allCardsRevealed: Bool;
+    private var pack: [PokemonCard];
+    
+    init(pack: [PokemonCard], imageService: ImageService, allCardsRevealed: Binding<Bool>) {
+        self.pack = pack;
+        self.imageService = imageService;
+        self._allCardsRevealed = allCardsRevealed;
+    }
+
+    var body: some View {
+        ZStack{
+            ForEach(self.pack.reversed(), id: \.id) { card in
+                CardImageView(uiImage: self.imageService.cardByImage[card] ?? UIImage()){
+                    self.zIndex += 1
+                    revealedCards.append(card);
+                    
+                    if(revealedCards.count == self.pack.count) {
+                        self.allCardsRevealed = true;
+                    }
+                    
+                    return self.zIndex
+                }
+            }
+            .gesture(
+                DragGesture()
+                    .onChanged { value in
+                        self.rotationOffset = value.translation
+                    }
+                    .onEnded { _ in
+                        withAnimation(
+                            .interactiveSpring(
+                                response: 0.6,
+                                dampingFraction: 0.32,
+                                blendDuration: 0.32))
+                        {
+                            self.rotationOffset = .zero
+                        }
+                    }
+            )
+            .rotation3DEffect(
+                self.offset2Angle(),
+                axis: (x: 1, y: 0, z: 2)
+            )
+            .rotation3DEffect(
+                self.offset2Angle(true),
+                axis: (x: 0, y: 1, z: 2)
+            )
+        }
+        .onAppear() {
+            
+        }
+    }
+    
+    func offset2Angle(_ isVerticle: Bool = false) -> Angle {
+        let degree = (isVerticle ? self.rotationOffset.height : self.rotationOffset.width) /
+            (isVerticle ? self.screenSize.height : self.screenSize.width)
+        return .init(degrees: degree * 20)
+    }
+    
+    var screenSize: CGSize = {
+        guard let window = UIApplication.shared.connectedScenes.first as? UIWindowScene
+        else {
+            return .zero
+        }
+        
+        return window.screen.bounds.size
+    }()
+}
--- PokemonCardSim/Views/PackView.swift
+++ PokemonCardSim/Views/PackView.swift
@@ -16,7 +16,6 @@ struct PackView: View {
     @State private var cardPack: [PokemonCard] = [];
     @State private var zIndex: Double = 0;
     @State private var rotationOffset: CGSize = .zero;
-    @State private var revealedCards: [PokemonCard] = [];
     @State private var allCardsRevealed: Bool = false;
     
     init(cardsByRarity: PokemonCardsByRarity, imageService: ImageService) {
@@ -26,42 +25,12 @@ struct PackView: View {
 
     var body: some View {
         ZStack{
-            ForEach(self.cardPack.reversed(), id: \.id) { card in
-                CardImageView(uiImage: self.imageService.cardByImage[card] ?? UIImage()){
-                    self.zIndex += 1
-                    revealedCards.append(card);
-                    
-                    if(revealedCards.count == self.cardPack.count) {
-                        self.allCardsRevealed = true;
-                    }
-                    
-                    return self.zIndex
-                }
+            if(!allCardsRevealed) {
+                PackOpener(pack: self.cardPack, imageService: self.imageService, allCardsRevealed: $allCardsRevealed)
+            } else {
+                Transition()
             }
-            .gesture(
-                DragGesture()
-                    .onChanged { value in
-                        self.rotationOffset = value.translation
-                    }
-                    .onEnded { _ in
-                        withAnimation(
-                            .interactiveSpring(
-                                response: 0.6,
-                                dampingFraction: 0.32,
-                                blendDuration: 0.32))
-                        {
-                            self.rotationOffset = .zero
-                        }
-                    }
-            )
-            .rotation3DEffect(
-                self.offset2Angle(),
-                axis: (x: 1, y: 0, z: 2)
-            )
-            .rotation3DEffect(
-                self.offset2Angle(true),
-                axis: (x: 0, y: 1, z: 2)
-            )
+            
         }
         .onAppear() {
             Task {
@@ -71,19 +40,4 @@ struct PackView: View {
             }
         }
     }
-    
-    func offset2Angle(_ isVerticle: Bool = false) -> Angle {
-        let degree = (isVerticle ? self.rotationOffset.height : self.rotationOffset.width) /
-            (isVerticle ? self.screenSize.height : self.screenSize.width)
-        return .init(degrees: degree * 20)
-    }
-    
-    var screenSize: CGSize = {
-        guard let window = UIApplication.shared.connectedScenes.first as? UIWindowScene
-        else {
-            return .zero
-        }
-        
-        return window.screen.bounds.size
-    }()
 }
